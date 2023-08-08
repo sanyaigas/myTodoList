ffe98ceb99b0767872a2bce0f7b6575d5d253bbb
Alex <sanyaigas88@gmail.com>
2023-08-08 22:16:55 +0300

    Самостоятельная 4.3 (Firebase: введение)

--- src/App.vue
+++ src/App.vue
@@ -19,14 +19,35 @@
       </div>
     </form>
 
-    <div v-for="todo in todos" :key="todo.id" class="card mb-5">
+    <div
+      v-for="todo in todos"
+      :key="todo.id"
+      class="card mb-5"
+      :class="{ 'has-background-success-light': todo.done }"
+    >
       <div class="card-content">
         <div class="content">
           <div class="columns is-mobile is-vcentered">
-            <div class="column">{{ todo.content }}</div>
+            <div
+              class="column"
+              :class="{ 'has-text-success line-through': todo.done }"
+            >
+              {{ todo.content }}
+            </div>
             <div class="column is-5 has-text-right">
-              <button class="button is-light">&check;</button>
-              <button class="button is-danger ml-2">&cross;</button>
+              <button
+                class="button"
+                :class="todo.done ? 'is-success' : 'is-light'"
+                @click="toggleDone(todo.id)"
+              >
+                &check;
+              </button>
+              <button
+                @click="deleteTodo(todo.id)"
+                class="button is-danger ml-2"
+              >
+                &cross;
+              </button>
             </div>
           </div>
         </div>
@@ -38,25 +59,24 @@
 <script setup>
 //import
 import { ref } from "vue";
-import { v4 as uuidv4 } from 'uuid';
+import { v4 as uuidv4 } from "uuid";
 
 //todo
 
 const todos = ref([
-  //   {
-  //     id: "id1",
-  //     content: "hello guys!",
-  //     done: false,
-  //   },
-  //   {
-  //     id: "id2",
-  //     content: "check this thing!",
-  //     done: false,
-  //   },
+//   {
+//     id: "id1",
+//     content: "hello guys!",
+//     done: false,
+//   },
+//   {
+//     id: "id2",
+//     content: "check this thing!",
+//     done: true,
+//   },
 ]);
 
 // add todo
-
 const newtodoContent = ref("");
 
 const addTodo = () => {
@@ -66,8 +86,22 @@ const addTodo = () => {
     done: false,
   };
   todos.value.unshift(newTodo);
-  newtodoContent.value = ""
+  newtodoContent.value = "";
 };
+
+//delete todo
+const deleteTodo = (id) => {
+  todos.value = todos.value.filter((todo) => todo.id !== id);
+};
+
+/*
+    toggle Dont
+*/
+const toggleDone = id => {
+    const index = todos.value.findIndex(todo => todo.id === id)
+    todos.value[index].done = !todos.value[index].done
+}
+
 </script>
 
 
@@ -78,5 +112,8 @@ const addTodo = () => {
   max-width: 400px;
   padding: 20px;
   margin: 0 auto;
+}
+.line-through {
+  text-decoration: line-through;
 }
 </style>
