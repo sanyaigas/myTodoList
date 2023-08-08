25b3cb788c818d0debd927faeeb7261b37a53c6a
Alex <sanyaigas88@gmail.com>
2023-08-08 23:57:17 +0300

    Самостоятельная 4.4 (Firebase: работа с Firestore DB)

--- src/App.vue
+++ src/App.vue
@@ -58,50 +58,79 @@
 
 <script setup>
 //import
-import { ref } from "vue";
-import { v4 as uuidv4 } from "uuid";
+import { ref, onMounted } from "vue";
+import {
+  collection,
+  onSnapshot,
+  addDoc,
+  doc,
+  deleteDoc,
+  updateDoc,
+} from "firebase/firestore";
+import { db } from "@/firebase";
+
+/**
+ * firebase ref
+ */
+
+const todosCollectionRef = collection(db, "todos");
 
 //todo
-
 const todos = ref([
-//   {
-//     id: "id1",
-//     content: "hello guys!",
-//     done: false,
-//   },
-//   {
-//     id: "id2",
-//     content: "check this thing!",
-//     done: true,
-//   },
+  //   {
+  //     id: "id1",
+  //     content: "hello guys!",
+  //     done: false,
+  //   },
+  //   {
+  //     id: "id2",
+  //     content: "check this thing!",
+  //     done: true,
+  //   },
 ]);
+
+//get todos
+onMounted(() => {
+  onSnapshot(todosCollectionRef, (querySnapshot) => {
+    const fbTodos = [];
+    querySnapshot.forEach((doc) => {
+      const todo = {
+        id: doc.id,
+        content: doc.data().content,
+        done: doc.data().done,
+      };
+      fbTodos.push(todo);
+    });
+    todos.value = fbTodos;
+  });
+});
 
 // add todo
 const newtodoContent = ref("");
 
 const addTodo = () => {
-  const newTodo = {
-    id: uuidv4(),
+  addDoc(todosCollectionRef, {
     content: newtodoContent.value,
     done: false,
-  };
-  todos.value.unshift(newTodo);
+  });
   newtodoContent.value = "";
 };
 
 //delete todo
 const deleteTodo = (id) => {
-  todos.value = todos.value.filter((todo) => todo.id !== id);
+  deleteDoc(doc(todosCollectionRef, id));
 };
 
-/*
-    toggle Dont
-*/
-const toggleDone = id => {
-    const index = todos.value.findIndex(todo => todo.id === id)
-    todos.value[index].done = !todos.value[index].done
-}
+/**
+ * toggle Dont
+ */
+const toggleDone = (id) => {
+  const index = todos.value.findIndex((todo) => todo.id === id);
 
+  updateDoc(doc(todosCollectionRef, id), {
+    done: !todos.value[index].done,
+  });
+};
 </script>
 
 
